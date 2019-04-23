6d681cdaa6a482d171e334698819d3db9971375d
kuro525 <deep.impact525-2012-5-pc@hotmail.co.jp>
2019-04-24 01:58:02 +0900

    [ADD] footerを新規作成

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
         <transition mode="out-in">
             <router-view></router-view>
         </transition>
+        <footer-component></footer-component>
         <!--<router-link to="/">Home</router-link> |-->
         <!--<router-link to="/about">About</router-link>-->
         <!--<router-view/>-->
@@ -12,11 +13,13 @@
 
 <script lang="ts">
     import HeaderComponent from "./views/Header.vue";
+    import FooterComponent from "./views/Footer.vue";
 
 
     export default {
         components: {
             HeaderComponent,
+            FooterComponent,
         },
     };
 </script>
